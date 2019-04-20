dc2ea2405d23cf8be16e5a03db9afb500268a3a7
姚帅 <yscode@126.com>
2019-04-20 11:37:28 +0800

    完善

--- ColvCellSelfSizing/ColvCellSelfSizing/FlowLayout_colv.swift
+++ ColvCellSelfSizing/ColvCellSelfSizing/FlowLayout_colv.swift
@@ -14,27 +14,31 @@ class FlowLayout_colv: UICollectionViewFlowLayout {
         // 预估itemSize
         estimatedItemSize = CGSize(width: (colV.bounds.width - 8 * 2) / 3, height: 36)
     }
-
+    
     // 向左对齐：正向遍历
     /*
-    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
-        guard let attrsArry = super.layoutAttributesForElements(in: rect) else {
-            return nil
-        }
-        for i in 0..<attrsArry.count {
-            if i < attrsArry.count - 1 {
-                let curAttr = attrsArry[i] // 当前attr
-                let nextAttr = attrsArry[i + 1]  // 下一个attr
-                if curAttr.frame.minY == nextAttr.frame.minY { // 如果下一个在同一行则调整，不在同一行则跳过
-                    if nextAttr.frame.minX - curAttr.frame.maxX > minimumInteritemSpacing{
-                        nextAttr.frame.origin.x = curAttr.frame.maxX + minimumInteritemSpacing
-                    }
-                }
-            }
-        }
-        return attrsArry
-    }
-    */
+     override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+     guard let attrsArry = super.layoutAttributesForElements(in: rect) else {
+     return nil
+     }
+     if attrsArry.count < 2{
+     return attrsArry
+     } else{
+     for i in 0..<attrsArry.count {
+     if i < attrsArry.count - 1 {
+     let curAttr = attrsArry[i] // 当前attr
+     let nextAttr = attrsArry[i + 1]  // 下一个attr
+     if curAttr.frame.minY == nextAttr.frame.minY { // 如果下一个在同一行则调整，不在同一行则跳过
+     if nextAttr.frame.minX - curAttr.frame.maxX > minimumInteritemSpacing{
+     nextAttr.frame.origin.x = curAttr.frame.maxX + minimumInteritemSpacing
+     }
+     }
+     }
+     }
+     return attrsArry
+     }
+     }
+     */
     
     // 向右对齐：倒序遍历
     override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
@@ -44,22 +48,30 @@ class FlowLayout_colv: UICollectionViewFlowLayout {
         guard let attrsArry = super.layoutAttributesForElements(in: rect) else {
             return nil
         }
-        for i in 0..<attrsArry.count {
-            let j = attrsArry.count - 1 - i
-            let curAttr = attrsArry[j]
-            if j == attrsArry.count - 1{ // 最后一个向右对齐
-                curAttr.frame.origin.x = colV.bounds.width - curAttr.frame.width
-            } else{
-                let nextAttr = attrsArry[j + 1]
-                if curAttr.frame.minY == nextAttr.frame.minY { // 如果下一个在同一行则调整
-                    if nextAttr.frame.minX - curAttr.frame.maxX > minimumInteritemSpacing{
-                        curAttr.frame.origin.x = nextAttr.frame.minX - minimumInteritemSpacing - curAttr.frame.width
-                    }
-                } else{ // 如果下一个不在同一行，则当前的向右对齐
+        if attrsArry.count == 0{
+            return attrsArry
+        } else if attrsArry.count == 1{
+            let curAttr = attrsArry[0]
+            curAttr.frame.origin.x = colV.bounds.width - curAttr.frame.width
+            return attrsArry
+        } else{
+            for i in 0..<attrsArry.count {
+                let j = attrsArry.count - 1 - i
+                let curAttr = attrsArry[j]
+                if j == attrsArry.count - 1{ // 最后一个向右对齐
                     curAttr.frame.origin.x = colV.bounds.width - curAttr.frame.width
+                } else{
+                    let nextAttr = attrsArry[j + 1]
+                    if curAttr.frame.minY == nextAttr.frame.minY { // 如果下一个在同一行则调整
+                        if nextAttr.frame.minX - curAttr.frame.maxX > minimumInteritemSpacing{
+                            curAttr.frame.origin.x = nextAttr.frame.minX - minimumInteritemSpacing - curAttr.frame.width
+                        }
+                    } else{ // 如果下一个不在同一行，则当前的向右对齐
+                        curAttr.frame.origin.x = colV.bounds.width - curAttr.frame.width
+                    }
                 }
             }
+            return attrsArry
         }
-        return attrsArry
     }
 }
